46ee3b4e340d0cfd8fd16479dd89f745541f3fc0
The Tech Guy <techguy.github@digitalmail.me>
2026-02-03 14:34:31 -0500

    Add in Hammerspoon config. Update Aerospace key mappings

--- mac-studio/.hammerspoon/init.lua
+++ mac-studio/.hammerspoon/init.lua
@@ -8,5 +8,30 @@ hs.hotkey.bind(hyper, "i", function()
     hs.application.launchOrFocus("Messages")
 end)
 
+-- Hyper + T: Launch Microsoft Teams
+hs.hotkey.bind(hyper, "t", function()
+    hs.application.launchOrFocus("Microsoft Teams")
+end)
+
+-- Hyper + E: Open new Finder window (floats via Aerospace)
+hs.hotkey.bind(hyper, "e", function()
+    hs.task.new("/usr/bin/open", nil, {"/Users/jamestucker"}):start()
+end)
+
+-- Hyper + M: Launch Mail
+hs.hotkey.bind(hyper, "m", function()
+    hs.application.launchOrFocus("Mail")
+end)
+
+-- Hyper + O: Launch Obsidian
+hs.hotkey.bind(hyper, "o", function()
+    hs.application.launchOrFocus("Obsidian")
+end)
+
+-- Hyper + Enter: Launch a new Kitty terminal
+hs.hotkey.bind(hyper, "return", function()
+    hs.task.new("/opt/homebrew/bin/kitty", nil):start()
+end)
+
 -- Reload config notification
 hs.alert.show("Hammerspoon config loaded")
